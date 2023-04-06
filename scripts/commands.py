a9fa8161efd7e63c71f7bad75536e042a9bcf434
algopapi <darrynbiervliet@gmail.com>
2023-04-06 18:56:09 +0200

    send/recieve messages

--- scripts/commands.py
+++ scripts/commands.py
@@ -2,17 +2,31 @@ import browse
 import json
 from memory import get_memory
 import datetime
+import json
+
 import agent_manager as agents
+import ai_functions as ai
+import browse
+import memory as mem
 import speak
 from config import Config
 import ai_functions as ai
-from file_operations import read_file, write_to_file, append_to_file, delete_file, search_files
+from file_operations import (
+    read_file,
+    write_to_file,
+    append_to_file,
+    delete_file,
+    search_files,
+)
 from execute_code import execute_python_file
 from json_parser import fix_and_parse_json
 from image_gen import generate_image
 from duckduckgo_search import ddg
+from execute_code import execute_python_file
+from file_operations import append_to_file, delete_file, read_file, write_to_file
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from json_parser import fix_and_parse_json
 
 cfg = Config()
 
@@ -24,18 +38,19 @@ def is_valid_int(value):
     except ValueError:
         return False
 
+
 def get_command(response):
     try:
         response_json = fix_and_parse_json(response)
-        
+
         if "command" not in response_json:
-            return "Error:" , "Missing 'command' object in JSON"
-        
+            return "Error:", "Missing 'command' object in JSON"
+
         command = response_json["command"]
 
         if "name" not in command:
             return "Error:", "Missing 'name' field in 'command' object"
-        
+
         command_name = command["name"]
 
         # Use an empty dictionary if 'args' field is not present in 'command' object
@@ -57,10 +72,11 @@ def execute_command(command_name, arguments):
 
     try:
         if command_name == "google":
-
             # Check if the Google API key is set and use the official search method
             # If the API key is not set or has only whitespaces, use the unofficial search method
-            if cfg.google_api_key and (cfg.google_api_key.strip() if cfg.google_api_key else None):
+            if cfg.google_api_key and (
+                cfg.google_api_key.strip() if cfg.google_api_key else None
+            ):
                 return google_official_search(arguments["input"])
             else:
                 return google_search(arguments["input"])
@@ -68,15 +84,15 @@ def execute_command(command_name, arguments):
             return memory.add(arguments["string"])
         elif command_name == "start_agent":
             return start_agent(
-                arguments["name"],
-                arguments["task"],
-                arguments["prompt"])
+                arguments["name"], arguments["task"], arguments["prompt"]
+            )
         elif command_name == "message_agent":
             return message_agent(arguments["key"], arguments["message"])
         elif command_name == "list_agents":
             return list_agents()
         elif command_name == "delete_agent":
             return delete_agent(arguments["key"])
+
         elif command_name == "get_text_summary":
             return get_text_summary(arguments["url"], arguments["question"])
         elif command_name == "get_hyperlinks":
@@ -116,8 +132,9 @@ def execute_command(command_name, arguments):
 
 
 def get_datetime():
-    return "Current date and time: " + \
-        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return "Current date and time: " + datetime.datetime.now().strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
 
 
 def google_search(query, num_results=8):
@@ -127,10 +144,12 @@ def google_search(query, num_results=8):
 
     return json.dumps(search_results, ensure_ascii=False, indent=4)
 
+
 def google_official_search(query, num_results=8):
+    import json
+
     from googleapiclient.discovery import build
     from googleapiclient.errors import HttpError
-    import json
 
     try:
         # Get the Google API key and Custom Search Engine ID from the config file
@@ -139,28 +158,37 @@ def google_official_search(query, num_results=8):
 
         # Initialize the Custom Search API service
         service = build("customsearch", "v1", developerKey=api_key)
-        
+
         # Send the search query and retrieve the results
-        result = service.cse().list(q=query, cx=custom_search_engine_id, num=num_results).execute()
+        result = (
+            service.cse()
+            .list(q=query, cx=custom_search_engine_id, num=num_results)
+            .execute()
+        )
 
         # Extract the search result items from the response
         search_results = result.get("items", [])
-        
+
         # Create a list of only the URLs from the search results
         search_results_links = [item["link"] for item in search_results]
 
     except HttpError as e:
         # Handle errors in the API call
         error_details = json.loads(e.content.decode())
-        
+
         # Check if the error is related to an invalid or missing API key
-        if error_details.get("error", {}).get("code") == 403 and "invalid API key" in error_details.get("error", {}).get("message", ""):
+        if error_details.get("error", {}).get(
+            "code"
+        ) == 403 and "invalid API key" in error_details.get("error", {}).get(
+            "message", ""
+        ):
             return "Error: The provided Google API key is invalid or missing."
         else:
             return f"Error: {e}"
 
     # Return the list of search result URLs
     return search_results_links
+
 
 def browse_website(url, question):
     summary = get_text_summary(url, question)
