cd6acc1ced9903fb586cf024f2f88e3f81a185da
algopapi <darrynbiervliet@gmail.com>
2023-04-14 18:03:03 +0200

    new prompt generator implementation

--- auto_gpt/data/prompts/__init__.py
+++
@@ -1,4 +0,0 @@
-from .response_prompt import (command, get_format_instructions, load_prompt,
-                              thought)
-
-__all__ = ["thought", "command", "load_prompt", "get_format_instructions"]
